949f5d054a1e1db2f17d130cc3dbbe4e8e55a627
shrijana <shrijana1991@gmail.com>
2021-03-28 02:48:53 +0545

    show claim button in detail page

--- static/js/21.c0ed58f4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[21],{860:function(n,f){}}]);
-//# sourceMappingURL=21.c0ed58f4.chunk.js.map
---
+++ static/js/22.11cd43a9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[22],{860:function(n,f){}}]);
+//# sourceMappingURL=22.11cd43a9.chunk.js.map
