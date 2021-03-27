48a04def49cc1005569af9f603d2b5d8f88eb1c8
shrijana <shrijana1991@gmail.com>
2021-03-27 12:17:01 +0545

    add detail page

--- static/js/20.e8f0c4c8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[20],{848:function(n,f){}}]);
-//# sourceMappingURL=20.e8f0c4c8.chunk.js.map
---
+++ static/js/21.39f2d5b8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[21],{851:function(n,f){}}]);
+//# sourceMappingURL=21.39f2d5b8.chunk.js.map
