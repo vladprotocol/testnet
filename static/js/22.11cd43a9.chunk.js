f61eca4d112128340096ab6a96aea982f95870c0
shrijana <shrijana1991@gmail.com>
2021-03-28 20:23:34 +0545

    align NFT in middle

--- static/js/22.11cd43a9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[22],{860:function(n,f){}}]);
-//# sourceMappingURL=22.11cd43a9.chunk.js.map
---
+++ static/js/22.2ad267c3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[22],{859:function(n,f){}}]);
+//# sourceMappingURL=22.2ad267c3.chunk.js.map
