3b9f9132d27e10bb2a475864d60a95c1ab9b32ab
shrijana <shrijana1991@gmail.com>
2021-03-29 14:02:33 +0545

    my collection

--- static/js/22.2ad267c3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[22],{859:function(n,f){}}]);
-//# sourceMappingURL=22.2ad267c3.chunk.js.map
---
+++ static/js/23.2d7fdb2e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[23],{859:function(n,f){}}]);
+//# sourceMappingURL=23.2d7fdb2e.chunk.js.map
