081c25eadabfc746232164918f130cfd86005213
shrijana <shrijana1991@gmail.com>
2021-04-02 16:00:00 +0545

    add detail pages for epic and legendary

--- static/js/25.15eaf12a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[25],{859:function(n,f){}}]);
-//# sourceMappingURL=25.15eaf12a.chunk.js.map
---
+++ static/js/27.f0b58373.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[27],{861:function(n,f){}}]);
+//# sourceMappingURL=27.f0b58373.chunk.js.map
