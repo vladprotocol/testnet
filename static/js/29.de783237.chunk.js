2a3f01942ce79e5b5d6986e0d4abc1817d9b7218
shrijana <shrijana1991@gmail.com>
2021-04-25 01:16:08 +0545

    external farms

--- static/js/29.de783237.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[29],{862:function(n,f){}}]);
-//# sourceMappingURL=29.de783237.chunk.js.map
---
+++ static/js/31.4034a72f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[31],{873:function(n,f){}}]);
+//# sourceMappingURL=31.4034a72f.chunk.js.map
