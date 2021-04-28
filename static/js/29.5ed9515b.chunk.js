05748e41141417729f8f06b77ca7aa414230dd90
shrijana <shrijana1991@gmail.com>
2021-04-28 13:14:14 +0545

    alife

---
+++ static/js/29.5ed9515b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpalife-frontend-farms"]=this["webpackJsonpalife-frontend-farms"]||[]).push([[29],{862:function(n,f){}}]);
+//# sourceMappingURL=29.5ed9515b.chunk.js.map
--- static/js/31.4034a72f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonplife-frontend-farms"]=this["webpackJsonplife-frontend-farms"]||[]).push([[31],{873:function(n,f){}}]);
-//# sourceMappingURL=31.4034a72f.chunk.js.map
